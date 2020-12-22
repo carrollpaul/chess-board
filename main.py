9e430440416007eec6917f7a4f248692da9effb5
Paul Carroll <pcarroll@zymergen.com>
2020-12-22 11:55:15 -0800

    ♟️

--- main.py
+++ main.py
@@ -20,40 +20,41 @@ DISCORD_SERVER_ID = int(os.getenv('DISCORD_SERVER_ID'))
 DISCORD_CHANNEL_ID = int(os.getenv('DISCORD_CHANNEL_ID'))
 CHESS_GAME_ID = os.getenv('CHESS_GAME_ID')
 
-# DISCORD STUFF HERE
+# Make game board
+board = chess.Board()
 
+# Setup lichess connection
+session = berserk.TokenSession(LICHESS_TOKEN)
+chess_client = berserk.Client(session)
+
+# Get game info
+game = chess_client.games.export(CHESS_GAME_ID)
+
+# DISCORD STUFF HERE
 discord_client = discord.Client()
 
 @discord_client.event
 async def on_ready():
-    channel = discord_client.get_channel(DISCORD_CHANNEL_ID)
     print(f'{discord_client.user} has connected to Discord!')
 
-@discord_client.event
-async def on_message(message):
     channel = discord_client.get_channel(DISCORD_CHANNEL_ID)
-    if message.author.id == discord_client.user.id:
-        return
-    if message.channel.id == DISCORD_CHANNEL_ID:
-        if message.content.startswith('!board'):
-            session = berserk.TokenSession(LICHESS_TOKEN)
-            chess_client = berserk.Client(session)
-            board = chess.Board()
-            game = chess_client.games.export(CHESS_GAME_ID)
-            moves = [move for move in game['moves'].split()]
+    stream = chess_client.board.stream_game_state(CHESS_GAME_ID)\
 
-            for move in moves:
-                board.push_san(move)
+    for event in stream:
+        moves = [move for move in event['state']['moves'].split()] # Get list of moves in game state
+        for move in moves:
+            board.push_san(move) # Add each move to the board
+        board_svg = chess.svg.board(board) 
+        with open('board.svg', 'w') as f:
+            f.write(board_svg) # Make .svg of board
+        with open('board.svg', 'rb') as f: 
+            board_png = cairosvg.svg2png(file_obj=f, write_to='board.png') # Convert .svg to .png
+        
+        white = event['white'].get('name') # Get white player
+        black = event['black'].get('name') # Get black player
 
-            board_svg = chess.svg.board(board)
-            with open('board.svg', 'w') as f:
-                f.write(board_svg)
-            with open('board.svg', 'rb') as f:
-                board_png = cairosvg.svg2png(file_obj=f, write_to='board.png')
-            await channel.send(file=discord.File('board.png'))
+        message = f'White pieces: {white}\nBlack pieces: {black}\nTurn #: {len(moves)}\n'
+        
+        await channel.send(content=message, file=discord.File('board.png')) # Post board image to Discord channel
 
 discord_client.run(DISCORD_TOKEN)
-
-
-# TODO: Include which player is which color in Discord post
-# TODO: Post image every time a move is made
